a4bc0cb77c5ac9c3cb626e5b7a156e5acf4e3e8c
Kukreja_ Rishabh (rikukreja@deloitte.com) <rikukreja@your.hostname.com>
2022-03-23 14:58:30 +0530

    added router

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { LinkContainer } from 'react-router-bootstrap'
 import { Container, Navbar, Nav } from 'react-bootstrap'
 
 const Header = () => {
@@ -6,20 +7,27 @@ const Header = () => {
     <header>
       <Navbar className='bg-color' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>
-            <b>PROFIND</b>
-          </Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand>
+              <b>PROFIND</b>
+            </Navbar.Brand>
+          </LinkContainer>
+
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ml-auto'>
-              <Nav.Link href='/login'>
-                <i class='fas fa-user'></i>
-                Sign In
-              </Nav.Link>
-              <Nav.Link href='/wishlist'>
-                <i class='fa-solid fa-heart'></i>
-                Wishlist
-              </Nav.Link>
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <i className='fas fa-user'></i>
+                  Sign In
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/wishlist'>
+                <Nav.Link>
+                  <i className='fa-solid fa-heart'></i>
+                  Wishlist
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
