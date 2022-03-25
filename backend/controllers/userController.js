2e8f4e9c08df568a745d6b4a29d05801ea586cd5
Kukreja_ Rishabh (rikukreja@deloitte.com) <rikukreja@your.hostname.com>
2022-03-25 20:41:01 +0530

    created json web token for user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
+import generateToken from '../utils/generateToken.js'
 
 // @desc Auth the user
 // @route POST /api/users/login
@@ -17,7 +18,7 @@ const authUser = asyncHandler(async (req, res) => {
       _id: user._id,
       name: user.name,
       email: user.email,
-      token: null,
+      token: generateToken(user._id),
     })
   } else {
     res.status(401)
