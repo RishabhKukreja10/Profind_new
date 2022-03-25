d7a4802d7657c5521ef1523c35d8a0235e765601
Kukreja_ Rishabh (rikukreja@deloitte.com) <rikukreja@your.hostname.com>
2022-03-25 13:28:18 +0530

    made seeder script to import initial users in our database

--- backend/data/users.js
+++ backend/data/users.js
@@ -4,21 +4,23 @@ const users = [
   {
     name: 'Rishabh',
     email: 'rishabh@gmail.com',
-    password: xxxx,
+    password: bcrypt.hashSync('12345', 10),
   },
   {
     name: 'Tushar',
     email: 'tushar@gmail.com',
-    password: xxxx,
+    password: bcrypt.hashSync('12345', 10),
   },
   {
     name: 'Gaurav',
     email: 'gaurav@gmail.com',
-    password: xxxx,
+    password: bcrypt.hashSync('12345', 10),
   },
   {
     name: 'Gaurangi',
     email: 'gaurangi@gmail.com',
-    password: xxxx,
+    password: bcrypt.hashSync('12345', 10),
   },
 ]
+
+export default users
