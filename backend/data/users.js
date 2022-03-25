a7587cd9fb39103bf7b9287918fa89590eb8716d
Kukreja_ Rishabh (rikukreja@deloitte.com) <rikukreja@your.hostname.com>
2022-03-25 13:07:01 +0530

    connected to databse profind and made database models

---
+++ backend/data/users.js
@@ -0,0 +1,24 @@
+import bcrypt from 'bcryptjs'
+
+const users = [
+  {
+    name: 'Rishabh',
+    email: 'rishabh@gmail.com',
+    password: xxxx,
+  },
+  {
+    name: 'Tushar',
+    email: 'tushar@gmail.com',
+    password: xxxx,
+  },
+  {
+    name: 'Gaurav',
+    email: 'gaurav@gmail.com',
+    password: xxxx,
+  },
+  {
+    name: 'Gaurangi',
+    email: 'gaurangi@gmail.com',
+    password: xxxx,
+  },
+]
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,20 +1,25 @@
 import mongoose from 'mongoose'
 
-const userSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-})
+  {
+    timestamps: true,
+  }
+)
 
 const User = mongoose.model('User', userSchema)
 
