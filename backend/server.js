3e06a38f7ac0757aa821fc00cadf42062039d56c
Kukreja_ Rishabh (rikukreja@deloitte.com) <rikukreja@your.hostname.com>
2022-03-25 19:11:10 +0530

    made user auth api

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
@@ -8,8 +9,12 @@ connectDB()
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
-  res.send('API is running')
+  res.send('API is running...')
 })
+
+app.use('/api/users', userRoutes)
 
 app.listen(5000, console.log('Server running on port 5000'))
